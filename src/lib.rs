a2b40d14a5518284cece209df4859fa05d147886
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-07-30 18:01:23 +0530

    Separate out lib and bin

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+use anyhow::Result;
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+pub async fn run() -> Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+    })
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,7 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, web};
 use anyhow::Result;
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use newsletter::run;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await?;
-    Ok(())
+    run().await
 }
