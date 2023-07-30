99257eb263833f5d72815126a9339b3bdeac4da0
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-07-30 18:28:22 +0530

    Write test for health check endpoint

---
+++ tests/health_check.rs
@@ -0,0 +1,20 @@
+fn spawn_app() {
+    let server = newsletter::run().expect("Failed to bind address.");
+    let _ = tokio::spawn(server);
+}
+
+#[tokio::test]
+async fn health_check() {
+    spawn_app();
+
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
