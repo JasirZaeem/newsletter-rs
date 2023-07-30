e67655fe2aca798a818dd15aef88140d6b7fc344
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-07-30 22:46:31 +0530

    Refactor routes into separate modules

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use newsletter::run;
+use newsletter::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<()> {
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,9 @@
+use newsletter::startup::run;
+
 fn spawn_app() -> String {
     let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
     let port = listener.local_addr().unwrap().port();
-    let server = newsletter::run(listener).expect("Failed to bind address.");
+    let server = run(listener).expect("Failed to bind address.");
     let _ = tokio::spawn(server);
 
     format!("http://127.0.0.1:{}", port)
