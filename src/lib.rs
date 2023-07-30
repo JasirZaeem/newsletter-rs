01db8539e24b075fb1364111e8289c362ebcad0c
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-07-30 18:37:24 +0530

    Refactor out tcp port binding from app runner

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,18 @@
 use actix_web::{App, HttpResponse, HttpServer, web};
 use actix_web::dev::Server;
 use anyhow::Result;
+use std::net::TcpListener;
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-pub fn run() -> Result<Server> {
+pub fn run(listener: TcpListener) -> Result<Server> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
     })
-        .bind("127.0.0.1:8000")?
+        .listen(listener)?
         .run();
     Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use newsletter::run;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    run()?.await?;
+    let listener = std::net::TcpListener::bind("127.0.0.1:8000")?;
+    run(listener)?.await?;
     Ok(())
 }
