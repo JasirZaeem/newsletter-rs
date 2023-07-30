4dfcf06333a246065f11485ae90d6897d67e4782
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-07-30 18:37:39 +0530

    Use random port to start app for test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,16 +1,20 @@
-fn spawn_app() {
-    let server = newsletter::run().expect("Failed to bind address.");
+fn spawn_app() -> String {
+    let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
+    let port = listener.local_addr().unwrap().port();
+    let server = newsletter::run(listener).expect("Failed to bind address.");
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
 
 #[tokio::test]
 async fn health_check() {
-    spawn_app();
+    let address = spawn_app();
 
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
